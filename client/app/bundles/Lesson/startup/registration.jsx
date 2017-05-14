c36fc86c81c62d116e5c354ff6411d64485a50cf
Matthew Black <mblackmblackmblack@gmail.com>
2017-05-14 14:51:38 -0400

    fix startup script

--- client/app/bundles/Lesson/startup/registration.jsx
+++ client/app/bundles/Lesson/startup/registration.jsx
@@ -1,6 +1,6 @@
 import ReactOnRails from 'react-on-rails';
 
-import Lesson from '../components/Lesson';
+import Lesson from '../components/Lesson/Lesson';
 
 // This is how react_on_rails can see the HelloWorld in the browser.
 ReactOnRails.register({
