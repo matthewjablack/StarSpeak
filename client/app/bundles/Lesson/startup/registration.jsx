6b0702817bd63a5c1ba2d2704f9f01f291f7bf16
Matthew Black <mblackmblackmblack@gmail.com>
2017-05-14 12:29:18 -0400

    update to lesson

---
+++ client/app/bundles/Lesson/startup/registration.jsx
@@ -0,0 +1,8 @@
+import ReactOnRails from 'react-on-rails';
+
+import Lesson from '../components/Lesson';
+
+// This is how react_on_rails can see the Lesson in the browser.
+ReactOnRails.register({
+  Lesson,
+});
