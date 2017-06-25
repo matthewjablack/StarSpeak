6a8dedb64b56b4d7615f55630abb3a9405665aeb
Matthew Black <mblackmblackmblack@gmail.com>
2017-06-25 12:30:49 -0400

    add ip information to privacy for logging

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,8 @@
 class PagesController < ApplicationController
 	def privacy
+		Rails.logger.info "Remote IP: " + request.remote_ip
+		Rails.logger.info "IP: " + request.ip
+		Rails.logger.info "Remote Address: " + request.env['REMOTE_ADDR']
 	end
 
 	def terms
