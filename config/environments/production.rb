196eef87670a10a8c632a51159b9129b5ed3fc02
Matthew Black <mblackmblackmblack@gmail.com>
2017-06-24 23:08:02 -0400

    fix google analytics

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,6 @@ StarSpeak::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  GA.tracker = "UA-101621947-1"
 end
