84fa8268cd28e29070cf688fee97f3213528adf7
Matthew Black <mblackmblackmblack@gmail.com>
2017-06-24 20:24:44 -0400

    update deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,6 +5,7 @@ set :application, 'StarSpeak'
 set :repo_url, 'git@github.com:mattBlackDesign/StarSpeak.git'
 
 set :deploy_to, '/home/deploy/StarSpeak'
+set :ssh_options, {:forward_agent => true}
 
 set :linked_files, %w{config/database.yml config/webapi.yml config/application.yml}
 set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
