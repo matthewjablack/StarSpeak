57e9fb8ce8b1a6515cb5e6bd9632ea1823adde39
Matthew Black <mblackmblackmblack@gmail.com>
2017-07-03 21:58:21 -0400

    finish basic watson hesitation

--- client/app/bundles/Lesson/components/Lesson/RenderResults.jsx
+++ client/app/bundles/Lesson/components/Lesson/RenderResults.jsx
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import {Stats} from './stats';
 import Pace from './Pace';
+import UmCount from './UmCount';
 
 export default class RenderResults extends Component {
   render() {
@@ -12,6 +13,7 @@ export default class RenderResults extends Component {
           <h1>Results</h1>
           <p>{this.props.local.stt}</p>
           <Pace pace={this.props.local.pace} />
+          <UmCount umCount={this.props.umCount} />
           <br/>
           <Stats stage={this.props.stage} indico={this.props.indico} watson={this.props.watson} user={this.props.user} screenshot={this.props.screenshot} local={this.props.local} />
           {this.props.children}
