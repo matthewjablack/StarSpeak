9508907055575d9fcdecb4c488186175f9f62fa0
Matthew Black <mblackmblackmblack@gmail.com>
2017-07-12 02:50:57 -0400

    add basic reset password

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -25,4 +25,21 @@ StarSpeak::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  config.action_mailer.raise_delivery_errors = true
+
+  config.action_mailer.default_url_options = { :host => "starspeak.io" }
+
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'starspeak.io',
+    user_name:            'info@starspeak.io',
+    password:             'starSpeak1895',
+    authentication:       'plain',
+    enable_starttls_auto: true  }
+
 end
