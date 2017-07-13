74460b25cbd1af0599f534f2d14dafc9fa849dde
Matthew Black <mblackmblackmblack@gmail.com>
2017-07-12 21:35:31 -0400

    user test info

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,20 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do 
+    @user = users(:john_black)
+    @user.save
+  end
+
+  test 'user name should come from first and last name' do 
+    assert_equal @user.first_name + ' ' + @user.last_name, @user.name
+  end
+
+  test 'user should have auth token' do 
+    assert_not_equal nil, @user.auth_token
+  end
+
+  test 'user admin should be false by default' do 
+    assert_equal false, @user.admin
+  end
 end
