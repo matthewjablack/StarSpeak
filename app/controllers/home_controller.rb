cd149678c12996642274af7e75b4c668ef7a98b6
Matthew Black <mblackmblackmblack@gmail.com>
2017-07-14 02:28:39 -0400

    fix level redirect

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -15,6 +15,8 @@ class HomeController < ApplicationController
   def dashboard
   	if !user_signed_in?
 			redirect_to root_path
+    else
+      redirect_to level_path(current_user.level)
 		end
   end
 
