f70d684cd44c464c842d811931ca9805dfdf8192
Matthew Black <mblackmblackmblack@gmail.com>
2017-07-15 03:11:05 -0400

    fix styling deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -30,31 +30,4 @@ namespace :deploy do
 
   after :publishing, 'deploy:restart'
   after :finishing, 'deploy:cleanup'
-
-  # after 'deploy:published', 'restart' do
-	 #  invoke 'delayed_job:restart'
-  # end
-
-
 end
-
-# namespace :delayed_job do
-
-#   desc "Install Deployed Job executable if needed"
-#   task :install do
-#     on roles(delayed_job_roles) do |host|
-#       within release_path do
-#         # Only install if not already present
-#         unless test("[ -f #{release_path}/#{delayed_job_bin} ]")
-#           with rails_env: fetch(:rails_env) do
-#             execute :bundle, :exec, :rails, :generate, :delayed_job
-#           end
-#         end
-#       end
-#     end
-#   end
-
-#   before :start, :install
-#   before :restart, :install
-
-# end
