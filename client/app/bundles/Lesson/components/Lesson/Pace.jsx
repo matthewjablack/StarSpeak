7bad294cfcc90f825bf6a2e1075a424ebbc5d152
Matthew Black <mblackmblackmblack@gmail.com>
2017-07-24 21:47:13 -0400

    add tests for pace

--- client/app/bundles/Lesson/components/Lesson/Pace.jsx
+++ client/app/bundles/Lesson/components/Lesson/Pace.jsx
@@ -5,15 +5,15 @@ export default class Pace extends Component {
   render() {
   	let paceInfo;
     if ((this.props.pace > 5) && (this.props.pace < 50)) {
-  		paceInfo = (<p className="red-txt">Your talking pace was very slow. Try working on reducing your pauses. </p>);
+  		paceInfo = (<p className="red-txt">Your talking pace was very slow. Try working on reducing your pauses.</p>);
   	} else if ((this.props.pace >= 50) && (this.props.pace < 120)) {
-  		paceInfo = (<p className="yellow-txt">Your talking pace was slightly slow. Be sure not to have too many pauses. </p>);
+  		paceInfo = (<p className="yellow-txt">Your talking pace was slightly slow. Be sure not to have too many pauses.</p>);
   	} else if ((this.props.pace >= 120) && (this.props.pace < 150)) {
   		paceInfo = (<p className="green-txt">Excellent talking pace. Keep it up!</p>);
-  	} else if ((this.props.pace >= 150) && (this.props.pace < 250)) {
+  	} else if ((this.props.pace >= 150) && (this.props.pace < 200)) {
   		paceInfo = (<p className="yellow-txt">Your talking pace was slightly fast. Try slowing down a little.</p>);
   	} else {
-  		paceInfo = (<p className="red-txt">You're talking way too fast! Try slowing down. </p>);
+  		paceInfo = (<p className="red-txt">You're talking way too fast! Try slowing down.</p>);
   	}
 
     if (this.props.pace > 5) {
--- client/karma.conf.js
+++ client/karma.conf.js
@@ -25,6 +25,11 @@ module.exports = function(config) {
     exclude: [
     ],
 
+    browserConsoleLogOptions: {
+      terminal: true,
+      level: ""
+    },
+
 
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
