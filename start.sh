ea1aa1005882e1c41f6d872bb02f3f7c578a3579
Matthew Black <mblackmblackmblack@gmail.com>
2017-08-07 18:59:33 -0400

    change dirname to pwd

--- start.sh
+++ start.sh
@@ -2,28 +2,28 @@
 osascript -e 'tell application "Terminal"
 	activate
 	if exists window 1 then
-        do script "cd dirname '$0'" in selected tab of the front window
+        do script "cd pwd" in selected tab of the front window
 	do script "redis-server /usr/local/etc/redis.conf" in selected tab of the front window
     else
-        do script "cd dirname '$0'"
+        do script "cd pwd"
 	do script "redis-server /usr/local/etc/redis.conf"
     end if
     delay 1
     tell application "System Events"
         keystroke "t" using {command down}
     end tell
-    do script "cd dirname '$0'" in selected tab of the front window
+    do script "cd pwd" in selected tab of the front window
     do script "bundle exec sidekiq" in selected tab of the front window
     delay 1
     tell application "System Events"
         keystroke "t" using {command down}
     end tell
-    do script "cd dirname '$0'" in selected tab of the front window
+    do script "cd pwd" in selected tab of the front window
     do script "yarn rails-server" in selected tab of the front window
     delay 1
     tell application "System Events"
         keystroke "t" using {command down}
     end tell
-    do script "cd dirname '$0'" in selected tab of the front window
+    do script "cd pwd" in selected tab of the front window
     do script "rails s" in selected tab of the front window
 end tell'
