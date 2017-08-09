c6b5ea105c53e6a774dc4f63de47833b77e690f9
Matthew Black <mblackmblackmblack@gmail.com>
2017-08-09 02:03:19 -0400

    remove grade level

--- client/app/bundles/Lesson/components/Lesson/RenderResults.jsx
+++ client/app/bundles/Lesson/components/Lesson/RenderResults.jsx
@@ -3,7 +3,6 @@ import PropTypes from 'prop-types';
 import {Stats} from './stats';
 import Pace from './Pace';
 import UmCount from './UmCount';
-import GradeLevel from './GradeLevel';
 
 export default class RenderResults extends Component {
   render() {
@@ -15,7 +14,6 @@ export default class RenderResults extends Component {
           <p>{this.props.local.stt}</p>
           <Pace pace={this.props.local.pace} />
           <UmCount umCount={this.props.umCount} />
-          <GradeLevel gradeScore={this.props.gradeScore} />
           <br/>
           <Stats stage={this.props.stage} indico={this.props.indico} watson={this.props.watson} user={this.props.user} screenshot={this.props.screenshot} local={this.props.local} />
           {this.props.children}
