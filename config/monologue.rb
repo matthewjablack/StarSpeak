85dc4fe72207fd56d9675a1ff0f07b86e9f3bb84
Matthew Black <mblackmblackmblack@gmail.com>
2017-10-22 21:27:29 -0400

    setup basic monologue blog

---
+++ config/monologue.rb
@@ -0,0 +1,10 @@
+Monologue.config do |config|
+  config.site_name = "My blog"
+  config.site_subtitle = "my own place online"
+  config.site_url = "http://example.com"
+
+  config.meta_description = "This is my blog about..."
+  config.meta_keyword = "music, fun"
+
+
+end
